c886c0da57cbd45b2dc1a46bfe7bcb309fcd52db
Daniil <parhomenkod1@ukr.net>
2025-05-18 23:09:35 +0300

    Deleted all cors except actual

--- middleware/cors.ts
+++ middleware/cors.ts
@@ -2,9 +2,6 @@ import cors from 'cors';
 
 const corsOptions = {
   origin: [
-    'http://localhost:3000',
-    'https://fitness-frontend-blush.vercel.app',
-    'https://fitness-frontend-6dzrux4s7-h1ghwalkers-projects.vercel.app',
     'https://fitness-frontend-9rggfvan4-h1ghwalkers-projects.vercel.app', // Добавляем новый домен Vercel
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Разрешаем нужные методы
