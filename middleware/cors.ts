fefebeacfa06605e749b21455aac56f5375ff1ad
Daniil <parhomenkod1@ukr.net>
2025-05-19 00:19:50 +0300

    Fixed cors again

--- middleware/cors.ts
+++ middleware/cors.ts
@@ -1,13 +1,23 @@
 import cors from 'cors';
 
-const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'https://fitness-frontend-blush.vercel.app', // Добавляем новый домен Vercel
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Разрешаем нужные методы
-  allowedHeaders: ['Content-Type'],
-  credentials: true,
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://fitness-frontend-blush.vercel.app',
+  'https://fitness-frontend-9rggfvan4-h1ghwalkers-projects.vercel.app',
+];
+
+const corsOptionsDelegate = function (req: any, callback: any) {
+  const origin = req.header('Origin');
+  if (allowedOrigins.includes(origin)) {
+    callback(null, {
+      origin: true,
+      credentials: true,
+    });
+  } else {
+    callback(null, {
+      origin: false,
+    });
+  }
 };
 
-export const corsMiddleware = cors(corsOptions);
+export const corsMiddleware = cors(corsOptionsDelegate);
