d25ea16e0c8488d919a425e12cbb93b855bd07e9
Daniil <parhomenkod1@ukr.net>
2025-05-18 23:14:19 +0300

    Added new domain for Vercel

--- middleware/cors.ts
+++ middleware/cors.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 const corsOptions = {
   origin: [
-    'https://fitness-frontend-9rggfvan4-h1ghwalkers-projects.vercel.app', // Добавляем новый домен Vercel
+    'https://fitness-frontend-blush.vercel.app/', // Добавляем новый домен Vercel
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Разрешаем нужные методы
   allowedHeaders: ['Content-Type'],
